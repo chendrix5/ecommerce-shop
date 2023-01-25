544c221c9047bb342306e9096d9c28cade710ba8
clay hendrix <clayhendrix5@gmail.com>
2023-01-25 14:28:21 -0600

    first commit

---
+++ e_commerce/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Navbar } from './components/navbar';
+import { Shop } from './pages/shop/shop'
+import { Cart } from './pages/cart/cart'
+
+function App() {
+  return ( <div className="App"> 
+  <Router>
+    <Navbar />
+
+    <Routes>
+      <Route path="/" element={<Shop> </Shop>}> </Route>
+      <Route path="/cart" element={<Cart></Cart>}> </Route>
+
+    </Routes>
+  </Router>
+  
+  </div>
+  );
+}
+    
+
+export default App;
---
+++ e_commerce/src/products.js
@@ -0,0 +1,67 @@
+import product1 from './assets/1.jpg'
+import product2 from './assets/2.webp'
+import product3 from './assets/3.jpg'
+import product4 from './assets/4.webp'
+import product5 from './assets/5.jpg'
+import product6 from './assets/6.webp'
+import product7 from './assets/7.jpg'
+import product8 from './assets/8.jpg'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "IPhone",
+        price: 999.0,
+        productImage: product1
+
+    },
+    {
+        id: 2,
+        productName: "Macbook",
+        price: 999.0,
+        productImage: product2
+
+    },
+    {
+        id: 3,
+        productName: "Camera",
+        price: 699.0,
+        productImage: product3
+
+    },
+    {
+        id: 4,
+        productName: "Jacket",
+        price: 54.0,
+        productImage: product4
+
+    },
+    {
+        id: 5,
+        productName: "Led lights",
+        price: 12.0,
+        productImage: product5
+
+    },
+    {
+        id: 6,
+        productName: " Groovy T-shirt",
+        price: 15.0,
+        productImage: product6
+
+    },
+    {
+        id: 7,
+        productName: "Plain T-Shirt",
+        price: 5.0,
+        productImage: product7
+
+    },
+    {
+        id: 8,
+        productName: "Funky hat",
+        price: 10.0,
+        productImage: product8
+
+    },
+]
